fd58d08fb559e3220412d3ec9f0e209d3662a37d
Sey Cheon <sey.cheon@contentful.com>
2019-04-13 14:24:48 +0200

    Add Travis deploy config

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Meow!
+            Meow! This is Sey's project for learning Docker
           </p>
           <a
             className="App-link"
